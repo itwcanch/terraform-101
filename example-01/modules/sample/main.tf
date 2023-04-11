af96146433fe571b58e5e352ec811f178c12b466
itwcanch <81386688+itwcanch@users.noreply.github.com>
2023-04-10 21:24:49 -0300

    Update main.tf

--- example-01/modules/sample/main.tf
+++ example-01/modules/sample/main.tf
@@ -1,10 +1,10 @@
 resource "google_compute_instance" "default" {
   name         = "${var.google_compute_instance_name}-${var.environtment}"
   machine_type = var.google_compute_instance_machine_type
-  zone         = "europe-west1-c"
+  zone         = "us-central1-a"
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-11"
     }
   }
   network_interface {
